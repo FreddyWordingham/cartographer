33c4c3a84a33dd7315421e08147d462b0aa189d2
Freddy Wordingham <freddy@digilab.co.uk>
2025-03-03 15:28:48 +0000

    Incremented photo dependency version

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use cartographer::Tiles;
+use photo::ImageRGBA;
+
+const TILE_SIZE: [usize; 2] = [3, 3];
+
+/// Read command line arguments.
+fn read_inputs() -> (String, [usize; 2]) {
+    let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", args.len());
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input_image> <output_resolution>", args[0]);
+        std::process::exit(1);
+    }
+    let example_image_filepath = &args[1];
+    let map_resolution = {
+        // In the form "widthxheight".
+        let s = &args[2];
+        let mut parts = s.split('x');
+        let width = parts.next().unwrap().parse::<usize>().unwrap();
+        let height = parts.next().unwrap().parse::<usize>().unwrap();
+        [height, width]
+    };
+
+    (example_image_filepath.to_string(), map_resolution)
+}
+
+fn main() {
+    let (example_image_filepath, _output_map_resolution) = read_inputs();
+
+    let example_map =
+        ImageRGBA::<u8>::load(example_image_filepath).expect("Failed to load example map image.");
+    println!("{}", example_map);
+
+    let mut tiles = Tiles::new(TILE_SIZE);
+    tiles.ingest(&example_map);
+}
